52e39e6eac9a8a76428d3fae8b3bfd6512eb88f0
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-10 21:43:20 -0500

    Configuraciones servidor express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hi since node he")
+const express = require("express");
+
+// Create server/app of express
+const app = express();
+
+// GET
+app.get('/', (req, res) => {
+    // res.send("Hello world");
+    res.status(200).json({
+        ok:true, 
+        msg: "Todo bien"
+    })
+})
+
+app.listen(4000, () => {
+    console.log(`Server run in port ${4000}`)
+})
