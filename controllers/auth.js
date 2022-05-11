8e622134185ffddd75ddc9d25e4b3394aaeb5834
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-10 22:56:50 -0500

    Configuracion de CORS y body de peticiones

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,18 @@
-const { response } = require("express")
+const { response, request } = require("express")
 
 
-const newAuth = (req, res = response) => {
+const newAuth = (req = request, res = response) => {
+    const { name, email, password} =  req.body;
+    console.log(name, email, password);
     return res.json({
         ok: true,
         msg: 'Crear nuevo usuario'
     })
 }
 const loginUser = (req, res)=>{
+    const { email, password } = req.body;
+    console.log(email, password)
+
     return res.json({
         ok: true,
         msg: 'Login user'
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+
 var pathsRouter = require('./routes/auth');
 
 
 // Create server/app of express
 const app = express();
+
+// CORS
+app.use( cors() );
+
+// Lectura y parse del body
+app.use( express.json() );
 
 // Paths, routing
 app.use('/api/auth', pathsRouter);
